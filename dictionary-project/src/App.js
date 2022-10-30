571b21d4be554428500b4463eb1dcb1d58f8d736
Izymryda <witebear200396@gmail.com>
2022-10-30 17:14:14 +0200

    add bootstrap

--- dictionary-project/src/App.js
+++ dictionary-project/src/App.js
@@ -1,11 +1,11 @@
-import "./logo.png";
+import logo from "./logo.png";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       Hello
-      <img></img>
+      <img src={logo} alt="logo" className="App-logo img-fluid"></img>
     </div>
   );
 }
