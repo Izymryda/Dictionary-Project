13801aad7a6ce20e7396d1ab62b947ac1072e644
Izymryda <witebear200396@gmail.com>
2022-10-30 21:49:57 +0200

    dictionary component

--- dictionary-project/src/App.js
+++ dictionary-project/src/App.js
@@ -1,13 +1,19 @@
 import logo from "./logo.png";
 import "./App.css";
+import Dictionary from "./Dictionary.js";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} alt="logo" className="App-logo img-fluid"></img>
-      </header>
-      Hello
+      <div className="container">
+        <header className="App-header">
+          <img src={logo} alt="logo" className="App-logo img-fluid"></img>
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="text-center">Coded by Tania Slotvinska</footer>
+      </div>
     </div>
   );
 }
