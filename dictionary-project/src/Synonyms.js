f68ccc09711d04f6d660de85cf3cd8be8312231c
Izymryda <witebear200396@gmail.com>
2022-11-23 19:20:59 +0200

    Synonims

--- dictionary-project/src/Synonyms.js
+++ dictionary-project/src/Synonyms.js
@@ -2,17 +2,18 @@ import React from "react";
 
 export default function Synonyms(props) {
   console.log(props.synonyms);
+  console.log("Hello");
   if (props.synonyms.length !== 0) {
-    <strong>Synonims: </strong>;
-    {
-      props.synonyms.map(function (synonym, index) {
-        return (
-          <div key={index}>
-            <span>{synonym}</span>
-          </div>
-        );
-      });
-    }
+    return (
+      <div>
+        <strong>Synonims: </strong>
+        <ul>
+          {props.synonyms.map(function (synonym, index) {
+            return <li key={index}>{synonym}</li>;
+          })}
+        </ul>
+      </div>
+    );
   } else {
     return null;
   }
