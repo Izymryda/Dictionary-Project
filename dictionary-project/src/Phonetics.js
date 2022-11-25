357ebb8d09316aee354b5291b2158e8daaca67ee
Izymryda <witebear200396@gmail.com>
2022-11-25 16:13:56 +0200

    correct value col for audio component

--- dictionary-project/src/Phonetics.js
+++ dictionary-project/src/Phonetics.js
@@ -5,7 +5,11 @@ export default function Phonetics(props) {
   console.log(props.phonetics);
   return (
     <div className="Phonetics row align-items-center">
-      <ReactAudioPlayer src={props.phonetics.audio} controls className="col" />{" "}
+      <ReactAudioPlayer
+        src={props.phonetics.audio}
+        controls
+        className="col-4"
+      />{" "}
       <span className="col">{props.phonetics.text}</span>
     </div>
   );
