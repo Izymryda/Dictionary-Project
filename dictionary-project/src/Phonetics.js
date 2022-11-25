022050de80f69d688079eb49ca809e6e65d3f053
Izymryda <witebear200396@gmail.com>
2022-11-25 16:11:23 +0200

    row & col phonetics

--- dictionary-project/src/Phonetics.js
+++ dictionary-project/src/Phonetics.js
@@ -4,8 +4,9 @@ import ReactAudioPlayer from "react-audio-player";
 export default function Phonetics(props) {
   console.log(props.phonetics);
   return (
-    <div className="Phonetics ">
-      <ReactAudioPlayer src={props.phonetics.audio} controls />
+    <div className="Phonetics row align-items-center">
+      <ReactAudioPlayer src={props.phonetics.audio} controls className="col" />{" "}
+      <span className="col">{props.phonetics.text}</span>
     </div>
   );
 }
